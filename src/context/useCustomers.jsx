54efe8277d2c300f21cb72097700573edaf096c6
Pavan Kumar Tikkani <pavan@prag-matic.com>
2025-06-20 18:53:49 +0530

    Update useCustomers.jsx

--- src/context/useCustomers.jsx
+++ src/context/useCustomers.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useState, useContext, useEffect } from "react";
-import { fetchCustomers } from "../services/customer";
+import { fetchCustomers } from "../services";
 import { useAuth } from "./useAuth";
 
 const CustomerContext = createContext(null);
